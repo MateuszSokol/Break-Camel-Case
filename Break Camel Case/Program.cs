81665360dae8ff14d7a8cd111ae478c3cddf75c0
mateusz1231 <matizek2@vp.pl>
2022-05-25 13:20:09 +0200

    initial commit

---
+++ Break Camel Case/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Break_Camel_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String word = "AAlaMaKota";
+
+            camelCase(ref word);
+
+
+            Console.WriteLine(word);
+
+
+        }
+        public static ref string camelCase(ref String word)
+        {
+            char[] wordAlphabet = word.ToCharArray();
+
+            StringBuilder buildWordWithSpaces = new StringBuilder();
+
+
+            for (int i = 0; i < wordAlphabet.Length; i++)
+            {
+                String b = wordAlphabet[i].ToString().ToUpper();
+                String c = wordAlphabet[i].ToString();
+                if (c.Equals(b) && i != 0)
+                {
+                    buildWordWithSpaces.Append(" ");
+                }
+                buildWordWithSpaces.Append(wordAlphabet[i]);
+            }
+
+            word = buildWordWithSpaces.ToString();
+
+            return ref word;
+        }
+    }
+}
